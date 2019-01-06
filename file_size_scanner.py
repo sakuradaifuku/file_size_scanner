23f17d9d11bd3cfd118fd3cbe2d1f601fcd6c072
sakuradaifuku <44629633+sakuradaifuku@users.noreply.github.com>
2019-01-06 11:45:38 +0900

    Add files via upload

---
+++ file_size_scanner.py
@@ -0,0 +1,14 @@
+import os, sys
+files = os.listdir()
+file_size = {}
+file_size_list = []
+
+for file in files:
+    file_size[file] = round((os.path.getsize(file) / 1024) / 1024, 2)
+    file_size_list.append(round((os.path.getsize(file) / 1024) / 1024, 0))
+
+for k, v in sorted(file_size.items(), key=lambda x: -x[1]):
+    print(str(k) + ": " + str(v) + " MB")
+
+print()
+print(os.getcwd() + 'の総消費容量は ' + str(sum(file_size_list)) + " MBです。")
